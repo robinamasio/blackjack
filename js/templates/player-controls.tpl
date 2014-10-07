ffcc543541deb59bd96da4fc653a8b8a03e67c1e
Robin Gabrielli <robin.gabrielli@gmail.com>
2014-10-07 00:10:29 -0400

    dealing & prompting
    I went a little further with this change set than I intended.  This
    change set addresses the following user stories:
    
    “As a dealer, I want to initially deal out two cards to the current
    player, So that we can begin the game”
    
    “As a player, I want to be prompted to hit or stand, So that I know
    when I have to act”
    
    “As a player, I want to know my current best possible score, So that I
    can decide whether to hit or stand”
    
    “As a player, I want to hit, So that I can increase my score”
    
    This required the addition of:
    
    - a basic CSS reset & clear fix, and some basic CSS styling to
    categorize the messages on the screen
    - additional attributes for cards: numeric values and a flag to denote
    an ace (“value” now stores the numeric value, while “level” stores a
    displayable name like “2” or “Jack”)
    - a deal() function in the Deck collection which deals a specified
    number of cards into a buffer hand
    - a Hand collection which includes a getScore() function that
    calculates the total points in the hand, accounting for aces
    - templates for the dealing of a single new card, and the player
    controls for “Hit” and “Stand”
    - handlers for all controls in MainView
    
    There is still no mechanism for standing, for knowing when the player
    has busted, or passing control to the dealer.

---
+++ js/templates/player-controls.tpl
@@ -0,0 +1,5 @@
+<div class="game info">{{ dealtTo }}'s score is {{ score }}</div>
+<div id="controls">
+	<div id="hit" class="game action">Hit</div>
+	<div id="stand" class="game action">Stand</div>
+</div>
--- js/views/MainView.js
+++ js/views/MainView.js
@@ -1,21 +1,73 @@
-define([
-	'jquery',
-	'underscore',
-	'backbone',
-	'handlebars',
-	'text!templates/main.tpl'
+define(['jquery', 'underscore', 'backbone', 'handlebars', 'collections/Deck', 'collections/Hand', 'text!templates/main.tpl', 'text!templates/new-card.tpl', 'text!templates/player-controls.tpl'
 ], 
-function($, _, Backbone, Handlebars, tpl) {
+function($, _, Backbone, Handlebars, Deck, Hand, tpl, newCardTpl, playerControlsTpl) {
 	var MainView = Backbone.View.extend({
+
 		template: Handlebars.compile(tpl),
+		newCardTemplate: Handlebars.compile(newCardTpl),
+		playerControlsTemplate: Handlebars.compile(playerControlsTpl),
+
 		el: $("#main"),
 
 		initialize: function() {
-			this.render();
+			// create a new deck and shuffle it
+			this.collection = new Deck();
+			this.collection.shuffleCards();
+			this.collection.list();
+
+			this.dealer = new Hand();
+			this.player = new Hand();
+			this.buffer = new Hand();   // the area that new cards get dealt into before they get put 
+										// into the dealer or player's hand.
+
+			this.render();			
 		},
 
 		render: function() {
 			this.$el.html(this.template());
+		},
+
+		events: {
+			'click	#deal': 	'onDealClicked',
+			'click 	#hit': 		'onHitClicked',
+			'click 	#stand': 	'onStandClicked', 
+		},
+
+		onDealClicked: function() {
+			$('#controls').remove();
+			this.dealCardsToPlayer(2);
+		},
+
+		onHitClicked: function() {
+			$('#controls').remove();
+			this.dealCardsToPlayer(1);			
+		},
+
+		onStandClicked: function() {
+
+		},
+
+		dealCardsToPlayer: function(numCards) {
+			this.collection.deal(numCards, this.buffer);
+			this.describeDeal('PLAYER', this.buffer);
+			this.player.add(this.buffer.toJSON());
+			this.buffer.reset();
+
+			this.$el.append(this.playerControlsTemplate({
+				dealtTo: 'PLAYER',
+				score: this.player.getScore()
+			}));
+
+		},
+
+		describeDeal: function(dealtTo, hand) {
+			for (var i = 0; i < hand.models.length; i++) {
+				var model = hand.at(i);
+				this.$el.append(this.newCardTemplate({
+					dealtTo: dealtTo,
+					name: model.getDisplayableName()
+				}));
+			}
 		}
 	});	
 	return MainView;
