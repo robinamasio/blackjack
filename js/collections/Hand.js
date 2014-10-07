250a53271db1f95a73bd8953baeb92ca90cff890
Robin Gabrielli <robin.gabrielli@gmail.com>
2014-10-07 01:34:58 -0400

    cleanup and some documentation
    In this change set, I cleaned up the code a little and added some brief
    documentation.  I found that I could get rid of the “app.js” file,
    which I had intended to use to define and manage an event publishing
    subsystem, but this turned out not to be necessary.

--- js/collections/Hand.js
+++ js/collections/Hand.js
@@ -2,21 +2,30 @@ define(['underscore', 'backbone', 'models/Card'],
 function(_, Backbone, Card){
 	var Hand = Backbone.Collection.extend({
 
+		/* 
+		 * This collection represents a hand, whether it's the PLAYER's or the DEALER's.
+		 */
+
 		model: Card,
 
 		getScore: function() {
+			// calculate the score of this hand, taking aces into account.
 			var total = 0;
 			var numAces = 0;
 			for (var i = 0; i < this.models.length; i++) {
 				var card = this.at(i);
 				if (!card.get('isAce')) {
+					// if this card is NOT an ace, simply add its numeric value to the total
 					total += card.get('value');
 				} else {
+					// if this card IS an ace, simply keep track of how many aces we have.
 					numAces += 1;
 				}
 			}
 
 			if (numAces > 0) {
+				// for each ace, assign it a value of 11 if doing so will not bust the player.  
+				// otherwise, assign it a value of 1.
 				for (var j = 0; j < numAces; j++) {
 					if (total + 11 <= 21) {
 						total += 11;
--- js/models/Card.js
+++ js/models/Card.js
@@ -2,12 +2,18 @@ define(['underscore', 'backbone'],
 function(_, Backbone){
 	var Card = Backbone.Model.extend({
 
+		/*
+		 * The Card model represents a single playing card, contains attributes which 
+		 * are supplied to it by the Deck collection.
+		 */
+
 		getDisplayableName: function() {
+			// return a nice-looking string which describes this card.
 			return this.toTitleCase(this.get('level')) + ' of ' + this.toTitleCase(this.get('suit'));
 		},
 
 		toTitleCase: function(str) {
-			// uses a simple closure to capitalize only the first letter of words
+			// uses a simple closure to capitalize only the first letter of words.
 		    return str.replace(/\w\S*/g, function(txt) {
 		    	return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 		    });
